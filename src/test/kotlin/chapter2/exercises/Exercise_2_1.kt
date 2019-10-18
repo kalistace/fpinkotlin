03417d9b46f22a202084e5f4c9a2381a4d540183
Cedric Kalista <ckalista@ebay.com>
2019-10-18 12:36:27 +0200

    Solution 2_1

--- src/test/kotlin/chapter2/exercises/Exercise_2_1.kt
+++ src/test/kotlin/chapter2/exercises/Exercise_2_1.kt
@@ -4,16 +4,30 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import kotlinx.collections.immutable.persistentMapOf
 
+//0, 1, 1, 2, 3, 5, 8, 13
 class Exercise_2_1 : WordSpec({
     // tag::init[]
-    fun fib(i: Int): Int = TODO()
+    //think inverse fibonacci sequence : 13, 8, 5, 3, 2, 1, 1, 0
+    fun fib(i: Int): Int {
+        fun fibAcc(n: Int, next: Int, current: Int): Int =
+                if (n == 0) current
+                else fibAcc(n - 1, current + next, next)
+        return fibAcc(i, 1, 0)
+    }
     // end::init[]
 
+    //0, 1, 1, 2, 3, 5, 8, 13
+    fun fibNonTailRec(i: Int): Int =
+            if (i == 0) {
+                0
+            } else {
+                fib(i - 2) + fib(i - 1)
+            }
     /**
      * Re-enable the tests by removing the `!` prefix!
      */
     "fib" should {
-        "!return the nth fibonacci number" {
+        "return the nth fibonacci number" {
             persistentMapOf(
                 Pair(1, 1),
                 Pair(2, 1),
