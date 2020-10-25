753871ba724ce17cf182fc6eab768790a7868789
Cedric Kalista <ckalista@gumtree.com>
2020-10-25 19:11:05 +0100

    Fix formatting

--- src/test/kotlin/chapter2/exercises/Exercise_2_2.kt
+++ src/test/kotlin/chapter2/exercises/Exercise_2_2.kt
@@ -22,7 +22,6 @@ fun <A> isSorted(aa: List<A>, ordered: (A, A) -> Boolean): Boolean {
             }
 
     return aa.isEmpty() || isSortedAcc(aa.head, aa.tail)
-
 }
 // end::init[]
 
@@ -32,18 +31,34 @@ class Exercise_2_2 : WordSpec({
      * Re-enable the tests by removing the `!` prefix!
      */
     "isSorted" should {
-        "detect ordering of a list of correctly ordered Ints based on an ordering HOF" {
-            isSorted(persistentListOf(1, 2, 3)) { a, b -> b > a } shouldBe true
+        """detect ordering of a list of correctly ordered Ints based
+            on an ordering HOF""" {
+            isSorted(
+                persistentListOf(1, 2, 3)
+            ) { a, b -> b > a } shouldBe true
         }
-        "detect ordering of a list of incorrectly ordered Ints based on an ordering HOF" {
-            isSorted(persistentListOf(1, 3, 2)) { a, b -> b > a } shouldBe false
+        """detect ordering of a list of incorrectly ordered Ints
+            based on an ordering HOF""" {
+            isSorted(
+                persistentListOf(1, 3, 2)
+            ) { a, b -> b > a } shouldBe false
         }
-        "verify ordering of a list of correctly ordered Strings based on an ordering HOF" {
-            isSorted(persistentListOf("a", "b", "c")) { a, b -> b > a } shouldBe true
+        """verify ordering of a list of correctly ordered Strings
+            based on an ordering HOF""" {
+            isSorted(
+                persistentListOf("a", "b", "c")
+            ) { a, b -> b > a } shouldBe true
         }
-        "verify ordering of a list of incorrectly ordered Strings based on an ordering HOF" {
-            isSorted(persistentListOf("a", "z", "w")) { a, b -> b > a } shouldBe false
+        """verify ordering of a list of incorrectly ordered Strings
+            based on an ordering HOF""" {
+            isSorted(
+                persistentListOf("a", "z", "w")
+            ) { a, b -> b > a } shouldBe false
+        }
+        "return true for an empty list" {
+            isSorted(persistentListOf<Int>()) { a, b ->
+                b > a
+            } shouldBe true
         }
     }
-
 })
--- src/test/kotlin/chapter2/exercises/Exercise_2_5.kt
+++ src/test/kotlin/chapter2/exercises/Exercise_2_5.kt
@@ -15,13 +15,12 @@ class Exercise_2_5 : WordSpec({
     "compose" should {
         "apply function composition over two functions" {
             val fahrenheit2celsius: (Double) -> String =
-                    compose<Double, Double, String>(
-                            { b -> "$b degrees celsius" },
-                            { a -> (a - 32.0) * (5.0 / 9.0) }
-                    )
+                compose<Double, Double, String>(
+                    { b -> "$b degrees celsius" },
+                    { a -> (a - 32.0) * (5.0 / 9.0) }
+                )
 
             fahrenheit2celsius(68.0) shouldBe "20.0 degrees celsius"
         }
     }
-
 })
