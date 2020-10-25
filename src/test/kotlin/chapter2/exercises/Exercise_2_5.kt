034ebed6ae147b90727150effc496516045e22e7
Cedric Kalista <ckalista@gumtree.com>
2020-10-25 18:24:20 +0100

    Add solution 2.5

--- src/test/kotlin/chapter2/exercises/Exercise_2_5.kt
+++ src/test/kotlin/chapter2/exercises/Exercise_2_5.kt
@@ -5,21 +5,23 @@ import io.kotlintest.specs.WordSpec
 
 class Exercise_2_5 : WordSpec({
     // tag::init[]
-    fun <A, B, C> compose(f: (B) -> C, g: (A) -> B): (A) -> C = TODO()
+    fun <A, B, C> compose(f: (B) -> C, g: (A) -> B): (A) -> C =
+            { a -> f(g(a)) }
     // end::init[]
 
     /**
      * Re-enable the tests by removing the `!` prefix!
      */
     "compose" should {
-        "!apply function composition over two functions" {
+        "apply function composition over two functions" {
             val fahrenheit2celsius: (Double) -> String =
-                compose<Double, Double, String>(
-                    { b -> "$b degrees celsius" },
-                    { a -> (a - 32.0) * (5.0 / 9.0) }
-                )
+                    compose<Double, Double, String>(
+                            { b -> "$b degrees celsius" },
+                            { a -> (a - 32.0) * (5.0 / 9.0) }
+                    )
 
             fahrenheit2celsius(68.0) shouldBe "20.0 degrees celsius"
         }
     }
+
 })
