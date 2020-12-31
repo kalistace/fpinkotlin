9afa276a3977513fd50499452cbdabefc6c0a87f
Cedric Kalista <ckalista@gumtree.com>
2020-12-31 15:00:39 +0100

    Add some solutions

--- src/test/kotlin/chapter5/exercises/Exercise_5_1.kt
+++ src/test/kotlin/chapter5/exercises/Exercise_5_1.kt
@@ -1,20 +1,31 @@
 package chapter5.exercises
 
 import chapter3.List
+import chapter3.Nil
+import chapter3.exercises.reverse
+import chapter5.Cons
+import chapter5.Empty
 import chapter5.Stream
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
+import chapter3.Cons as ConsList
 
 /**
  * Re-enable the tests by removing the `!` prefix!
  */
 class Exercise_5_1 : WordSpec({
     //tag::init[]
-    fun <A> Stream<A>.toList(): List<A> = TODO()
+    fun <A> Stream<A>.toList(): List<A> {
+        tailrec fun iterate(xs: Stream<A>, acc: List<A>): List<A> = when (xs) {
+            is Empty -> acc
+            is Cons -> iterate(xs.tail(), ConsList(xs.head(), acc))
+        }
+        return reverse(iterate(this, Nil))
+    }
     //end::init[]
 
     "Stream.toList" should {
-        "!force the stream into an evaluated list" {
+        "force the stream into an evaluated list" {
             val s = Stream.of(1, 2, 3, 4, 5)
             s.toList() shouldBe List.of(1, 2, 3, 4, 5)
         }
--- src/test/kotlin/chapter5/exercises/Exercise_5_2.kt
+++ src/test/kotlin/chapter5/exercises/Exercise_5_2.kt
@@ -2,7 +2,11 @@ package chapter5.exercises
 
 import chapter3.List
 import chapter3.Nil
+import chapter5.Cons
+import chapter5.Empty
 import chapter5.Stream
+import chapter5.Stream.Companion.cons
+import chapter5.Stream.Companion.empty
 import chapter5.solutions.toList
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
@@ -13,43 +17,50 @@ import io.kotlintest.specs.WordSpec
 class Exercise_5_2 : WordSpec({
 
     //tag::take[]
-    fun <A> Stream<A>.take(n: Int): Stream<A> = TODO()
+    fun <A> Stream<A>.take(n: Int): Stream<A> = when (this) {
+        Empty -> empty()
+        is Cons -> if (n == 0) empty() else cons(this.head,
+            { this.tail().take(n - 1) })
+    }
     //end::take[]
 
     //tag::drop[]
-    fun <A> Stream<A>.drop(n: Int): Stream<A> = TODO()
+    fun <A> Stream<A>.drop(n: Int): Stream<A> = when (this) {
+        Empty -> empty()
+        is Cons -> if (n == 0) this else this.tail().drop(n-1)
+    }
     //end::drop[]
 
     "Stream.take(n)" should {
-        "!return the first n elements of a stream" {
+        "return the first n elements of a stream" {
             val s = Stream.of(1, 2, 3, 4, 5)
             s.take(3).toList() shouldBe List.of(1, 2, 3)
         }
 
-        "!return all the elements if the stream is exhausted" {
+        "return all the elements if the stream is exhausted" {
             val s = Stream.of(1, 2, 3)
             s.take(5).toList() shouldBe List.of(1, 2, 3)
         }
 
-        "!return an empty stream if the stream is empty" {
-            val s = Stream.empty<Int>()
+        "return an empty stream if the stream is empty" {
+            val s = empty<Int>()
             s.take(3).toList() shouldBe Nil
         }
     }
 
     "Stream.drop(n)" should {
-        "!return the remaining elements of a stream" {
+        "return the remaining elements of a stream" {
             val s = Stream.of(1, 2, 3, 4, 5)
             s.drop(3).toList() shouldBe List.of(4, 5)
         }
 
-        "!return empty if the stream is exhausted" {
+        "return empty if the stream is exhausted" {
             val s = Stream.of(1, 2, 3)
             s.drop(5).toList() shouldBe Nil
         }
 
-        "!return empty if the stream is empty" {
-            val s = Stream.empty<Int>()
+        "return empty if the stream is empty" {
+            val s = empty<Int>()
             s.drop(5).toList() shouldBe Nil
         }
     }
