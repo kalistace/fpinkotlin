4776fe484aaf25ab417524c59208a4ad925ad98a
Cedric Kalista <ckalista@gumtree.com>
2021-01-05 23:33:15 +0100

    Add some solutions

--- src/test/kotlin/chapter5/exercises/Exercise_5_12.kt
+++ src/test/kotlin/chapter5/exercises/Exercise_5_12.kt
@@ -14,44 +14,48 @@ import io.kotlintest.specs.WordSpec
 class Exercise_5_12 : WordSpec({
 
     //tag::fibs[]
-    fun fibs(): Stream<Int> = unfold(Pair(0, 1),
-        { s -> Some(Pair(s.first, Pair(s.second, s.first + s.second))) })
+    fun fibs(): Stream<Int> = unfold(Pair(0, 1)) { s ->
+        Some(Pair(s.first,
+            Pair(s.second, s.first + s.second)))
+    }
     //end::fibs[]
 
     //tag::from[]
-    fun from(n: Int): Stream<Int> = TODO()
+    fun from(n: Int): Stream<Int> =
+        unfold(n) { s -> Some(Pair(s, s + 1)) }
     //end::from[]
 
     //tag::constant[]
-    fun <A> constant(n: A): Stream<A> = TODO()
+    fun <A> constant(n: A): Stream<A> =
+        unfold(n) { s -> Some(Pair(s, s)) }
     //end::constant[]
 
     //tag::ones[]
-    fun ones(): Stream<Int> = TODO()
+    fun ones(): Stream<Int> = unfold(1) { s -> Some(Pair(s, s)) }
     //end::ones[]
 
     "fibs" should {
-        "!return a Stream of fibonacci sequence numbers" {
+        "return a Stream of fibonacci sequence numbers" {
             fibs().take(10).toList() shouldBe
                 List.of(0, 1, 1, 2, 3, 5, 8, 13, 21, 34)
         }
     }
 
     "from" should {
-        "!return a Stream of ever incrementing numbers" {
+        "return a Stream of ever incrementing numbers" {
             from(5).take(5).toList() shouldBe List.of(5, 6, 7, 8, 9)
         }
     }
 
     "constants" should {
-        "!return an infinite stream of a given value" {
+        "return an infinite stream of a given value" {
             constant(1).take(5).toList() shouldBe
                 List.of(1, 1, 1, 1, 1)
         }
     }
 
     "ones" should {
-        "!return an infinite stream of 1s" {
+        "return an infinite stream of 1s" {
             ones().take(5).toList() shouldBe List.of(1, 1, 1, 1, 1)
         }
     }
